511bb9e35df48d1750390e9e7593a512b243481c
Daniel Marshall <d.marshall@outlook.com>
2025-02-21 23:04:20 +0000

    Digest JSON

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,7 @@ abstract class DockerBuildTask
         var shaDigest = ByteArrayOutputStream().use { stream ->
             execOperations.exec {
                 standardOutput = stream
-                commandLine("docker", "inspect", "--format=\"{{index .RepoDigests 0}}\"", tag)
+                commandLine("docker", "inspect", "--format=\"{{json .Manifest}}\"", tag)
             }
 
             stream.toString();
